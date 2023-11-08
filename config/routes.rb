d2ad303cc67b109a1d5d971c122bf6f4ae4a3c07
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-11-08 11:08:55 +0000

    Fix root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "hangouts/index"
+  root "hangouts#index"
   get 'hangouts/index'
   devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
