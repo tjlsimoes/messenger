4eeea68fcb61148bad260e6de3d05214569a81d0
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-11-08 14:40:27 +0000

    Direct user input for rebroadcasting

--- app/javascript/channels/message_channel.js
+++ app/javascript/channels/message_channel.js
@@ -1,6 +1,6 @@
 import consumer from "channels/consumer"
 
-consumer.subscriptions.create("MessageChannel", {
+const messageChannel = consumer.subscriptions.create("MessageChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
   },
@@ -13,3 +13,19 @@ consumer.subscriptions.create("MessageChannel", {
     // Called when there's incoming data on the websocket for this channel
   }
 });
+
+document.addEventListener("turbo:load", () => {
+  let form = document.querySelector('#message-form')
+  if(form) {
+    form.addEventListener('submit', (e) => {
+      console.log("Test A")
+      e.preventDefault()
+      let messageInput = document.querySelector('#message-input').value
+      if(messageInput == '') return;
+      const message = {
+        body: messageInput
+      }
+      messageChannel.send({message: message})
+    })
+  }
+})
