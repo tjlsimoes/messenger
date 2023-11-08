502c86b3c582eaa2ade2c66d4fe190c6e14c78f5
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-11-08 14:58:02 +0000

    Add Message model and setup User accordingly

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "hangouts#index"
-  get 'hangouts/index'
   devise_for :users
+  resources :messages, only: [:create]
   get "up" => "rails/health#show", as: :rails_health_check
 end
